90c99fe649358a3c52f2309609958d89c34f7cd9
Arushi Sharma <arushisharma@zapbuild.com>
2021-10-11 15:13:14 +0530

    changes in homepage

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,5 +2,6 @@ class HomesController < ApplicationController
  # before_action :authenticate_user_presence!
 
 	def index
+		 @products = Product.paginate(page: params[:page], per_page: 5) 
 	end 
 end
--- app/models/favourite.rb
+++ app/models/favourite.rb
@@ -1,4 +1,6 @@
 class Favourite < ApplicationRecord
   belongs_to :user
   belongs_to :product
+
+  
 end
